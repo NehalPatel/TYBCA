7c723b9fc82b248b216f72b50a0f4328e72749e0
Nehal Patel <patelnehal4u@gmail.com>
2022-06-27 23:46:07 +0530

    changes

---
+++ SEM 5/501-02 - Advanced Mobile Computing/Playground/Practical/src/main/kotlin/Unit1-6.kts
@@ -0,0 +1,69 @@
+fun main(){
+
+    val marks = arrayOf(80,85,60,90,70)
+
+    //for loop
+    for(item in marks){
+        println(item)
+    }
+
+    //while loop
+//    var i = 1
+//    while (i<=5){
+//        println(i)
+//        i++
+//    }
+//
+//    //do while loop
+//    var j = 1
+//    do {
+//        println(j)
+//        j++
+//    }
+//    while (j<=5);
+//
+//    //break statement
+//
+//    for (i in 1..5) {
+//        if (i == 3) {
+//            break
+//        }
+//        println(i)
+//    }
+//
+//    loop@ for (i in 1..3) {
+//        for (j in 1..3) {
+//            println("i = $i and j = $j")
+//            if (i == 2)
+//                break@loop
+//        }
+//    }
+//
+//    //continue example
+//    for (i in 1..3) {
+//        println("i = $i")
+//        if (j == 2) {
+//            continue
+//        }
+//        println("this is below if")
+//    }
+//
+//    //label with continue statement
+//    start@ for (i in 1..3) {
+//        for (j in 1..3) {
+//            println("i = $i and j = $j")
+//            if (i == 2) {
+//                continue@start
+//            }
+//            println("this is below if")
+//        }
+//    }
+//
+//    // =====================================
+//
+//    var number = 25
+//    var result = Math.sqrt(number.toDouble())
+//    print("Square root of $number is $result")
+
+
+}
